f549ba1cea7094077687e9a09039da75970fd997
jjhun0712 <jjhun0712@naver.com>
2023-11-23 21:29:10 +0900

    Create Print_wit_string_attached_*.c

---
+++ Print_with_string_attached_1.c
@@ -0,0 +1,16 @@
+// 채점 시 7점 받음;;
+#include <stdio.h>
+#define LEN_INPUT1 11
+#define LEN_INPUT2 11
+#define LEN_INPUT3 21
+
+int main(void) {
+    char s1[LEN_INPUT1];
+    char s2[LEN_INPUT2];
+    char s3[LEN_INPUT3];
+    scanf("%s %s", s1, s2);
+    
+    printf("%s%s", s1,s2);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -7,3 +7,5 @@ a와 b 출력하기 : Print_a_and_b.c
 문자열 반복해서 출력하기 : Repeated_string_output.c
 대소문자 바꿔서 출력하기 : Print_letter_case.c
 특수문자 출력하기 : Print_special_characters.c
+문자열 붙여서 출력하기 : Print_with_string_attached_1.c
+                       Print_with_string_attached_2.c
